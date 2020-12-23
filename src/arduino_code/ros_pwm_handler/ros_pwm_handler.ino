d501cd80c274edb7980d3f58c56f2f40a6a84683
robot_nerds_wsu <lloyd@wayne.edu>
2020-12-23 18:01:31 -0500

    fixed pwm_handler arduino pin assignments and direction. set ticker/meter by measurement. PID_velocoity node outputting ridiculously high velocities.. check on that next

--- src/arduino_code/ros_pwm_handler/ros_pwm_handler.ino
+++ src/arduino_code/ros_pwm_handler/ros_pwm_handler.ino
@@ -11,20 +11,25 @@
 #include <std_msgs/Float32.h>
 #include <std_msgs/Int16.h>
 
-#define PWM_RIGHT 6   // PWM output pin for right motor
-#define DIR_RIGHT 3   // Direction output pin for right motor
+#define PWM_RIGHT 5 //6   // PWM output pin for right motor
+#define DIR_RIGHT 3 // 3   // Direction output pin for right motor
 
-#define PWM_LEFT 5    // PWM output pin for left motor
-#define DIR_LEFT 2    // Direction output pin for left motor
+#define PWM_LEFT 6 //5    // PWM output pin for left motor
+#define DIR_LEFT 2 //2    // Direction output pin for left motor
 #define MIN_PWM 50    //program outputs zero below this value to avoid energized motor below the movement threshold
 #define MAX_PWM 150   //adjust to set max speed
 #define PWM_CHANGE_INCREMENT 2  //adjust to change how quickly output changes
+
+int left_cmd = 0;
+int right_cmd = 0;
+
+ros::NodeHandle nh;
 
 std_msgs::Int16 left;
 std_msgs::Int16 right;
 ros::Publisher pubLeft("leftFromArduino", &left);
 ros::Publisher pubRight("rightFromArduino", &right);
-ros::NodeHandle nh;
+
 
 bool get_requested_direction(int requested_pwm)
 {
@@ -44,61 +49,39 @@ bool is_direction_change(int requested_pwm, int last_pwm)
 //this section increments PWM changes instead of making jarring/dangerous sudden big changes
 int get_new_pwm_out(int requestedPwm, int lastPwm)
 {
-
+  int newPWM = 0;
   if (is_direction_change(requestedPwm, lastPwm))
     requestedPwm = 0;
 
-  if (abs(requestedPwm) > lastPwm)
-    return lastPwm + PWM_CHANGE_INCREMENT;
-  else if (abs(requestedPwm) < lastPwm)
-    return lastPwm - PWM_CHANGE_INCREMENT;
+  if (requestedPwm > lastPwm)
+  {
+    newPWM = lastPwm + PWM_CHANGE_INCREMENT;
+  }
+  else if (requestedPwm < lastPwm)
+  {
+    newPWM = lastPwm - PWM_CHANGE_INCREMENT;
+  }
   else
-    return requestedPwm;
-}
+    newPWM = requestedPwm;
 
+  return newPWM;
+}
 //this happens when right wheel cmd message is recieved
 void rightMessageCb(const std_msgs::Float32 &request_pwm)
 {
-
-  static int lastPwm = 0;
-  int requestedPwm = (int)request_pwm.data;
-
-  //set direction pins - invert get_requested_dir() output if motor runs backwards
-  digitalWrite(DIR_RIGHT, !get_requested_direction(requestedPwm));
-
-  //update pwm value that will actually be written
-  lastPwm = get_new_pwm_out(requestedPwm, lastPwm);
-
-  //actually output pwm values
-  analogWrite(PWM_RIGHT, lastPwm);
-  right.data = PWM_RIGHT;
-  pubRight.publish(&right);
-
+  right_cmd = (int)request_pwm.data;
 }
 
 //this happens when left wheel cmd message is recieved
 void leftMessageCb(const std_msgs::Float32 &request_pwm)
 {
-  static int lastPwm = 0;
-  int requestedPwm = (int)request_pwm.data;
-
-  //set direction pins - invert get_requested_dir() output if motor runs backwards
-  digitalWrite(DIR_LEFT, !get_requested_direction(requestedPwm));
-
-  //update pwm value that will actually be written
-  lastPwm = get_new_pwm_out(requestedPwm, lastPwm);
-
-  //actually output pwm values
-  analogWrite(PWM_LEFT, lastPwm);
-  left.data = PWM_LEFT;
-  pubLeft.publish(&left);
-
+  left_cmd = (int)request_pwm.data;
 }
+
 
 //subscribe to cmd_vel
 ros::Subscriber<std_msgs::Float32> subRight("rmotor_pwm_cmd", &rightMessageCb);
 ros::Subscriber<std_msgs::Float32> subLeft("lmotor_pwm_cmd", &leftMessageCb);
-
 
 void setup()
 {
@@ -133,8 +116,41 @@ void setup()
 
 }
 
+
+
 void loop()
 {
   nh.spinOnce();
-  delay(20);
+  static int lastLeft = 0;
+  static int lastRight = 0;
+
+  left_cmd = (is_direction_change(left_cmd, lastLeft))    ? 0 : left_cmd;
+  right_cmd = (is_direction_change(right_cmd, lastRight)) ? 0 : right_cmd;
+
+
+  //update pwm value that will actually be written
+  int nextLeft = get_new_pwm_out(left_cmd, lastLeft);
+  int nextRight = get_new_pwm_out(right_cmd, lastRight);
+
+
+  //set direction pins - invert get_requested_dir() output if motor runs backwards
+  digitalWrite(DIR_LEFT, get_requested_direction(nextLeft));
+  digitalWrite(DIR_RIGHT, get_requested_direction(nextRight));
+
+
+  //actually output pwm values
+  analogWrite(PWM_LEFT, nextLeft);
+  analogWrite(PWM_RIGHT, nextRight);
+
+  lastLeft = nextLeft;
+  lastRight = nextRight;
+
+  std_msgs::Int16 l;
+  l.data = nextLeft;
+  pubLeft.publish(&l);
+
+  std_msgs::Int16 r;
+  r.data = lastRight;
+  pubRight.publish(&r);
+
 }
